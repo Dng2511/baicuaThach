5d81bdad7e355b5d291cb46cdc510ee5431284fd
Dng <phamdong9854@gmail.com>
2024-09-19 23:54:16 +0700

    edit login

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Link from "next/link";
 import Image from 'next/image'
 import MobileMenu from '../MobileMenu/MobileMenu'
-import { totalPrice } from "../../utils";
-import { connect } from "react-redux";
-import { removeFromCart } from "../../store/actions/action";
 
 
 
 const Header = (props) => {
-    const [menuActive, setMenuState] = useState(false);
-    const [cartActive, setcartState] = useState(false);
 
-    const SubmitHandler = (e) => {
-        e.preventDefault()
-    }
 
     const ClickHandler = () => {
         window.scrollTo(10, 0);
     }
-    const { carts } = props;
 
     return (
         <header id="header">
@@ -49,76 +40,13 @@ const Header = (props) => {
                                         <li><Link onClick={ClickHandler} href="/project">Project</Link></li>
                                         <li><Link onClick={ClickHandler} href="/blog">Blog</Link></li>
                                         <li><Link onClick={ClickHandler} href="/">Pages</Link></li>
-                                        <li><Link onClick={ClickHandler} href="/login">Login</Link></li>
-                                        <li><Link onClick={ClickHandler} href="/register">Register</Link></li>
                                         <li><Link onClick={ClickHandler} href="/contact">Contact</Link></li>
                                     </ul>
                                 </div>
                             </div>
                             <div className="col-lg-2 col-md-2 col-2">
-                                <div className="header-right">
-                                    <div className="header-search-form-wrapper">
-                                        <div className="cart-search-contact">
-                                            <button onClick={() => setMenuState(!menuActive)} className="search-toggle-btn"><i
-                                                className={`fi ti-search ${menuActive ? "ti-close" : "fi "}`}></i></button>
-                                            <div className={`header-search-form ${menuActive ? "header-search-content-toggle" : ""}`}>
-                                                <form onSubmit={SubmitHandler}>
-                                                    <div>
-                                                        <input type="text" className="form-control"
-                                                            placeholder="Search here..." />
-                                                        <button type="submit"><i
-                                                            className="fi ti-search"></i></button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="mini-cart">
-                                        <button className="cart-toggle-btn" onClick={() => setcartState(!cartActive)}>
-                                            {" "}
-                                            <i className="fi flaticon-shopping-cart"></i>{" "}
-                                            <span className="cart-count">{carts.length}</span>
-                                        </button>
-                                        <div className={`mini-cart-content ${cartActive ? "mini-cart-content-toggle" : ""}`}>
-                                            <button className="mini-cart-close" onClick={() => setcartState(!cartActive)}><i className="ti-close"></i></button>
-                                            <div className="mini-cart-items">
-                                                {carts &&
-                                                    carts.length > 0 &&
-                                                    carts.map((catItem, crt) => (
-                                                        <div className="mini-cart-item clearfix" key={crt}>
-                                                            <div className="mini-cart-item-image">
-                                                                <span>
-                                                                    <img src={catItem.proImg} alt="icon" />
-                                                                </span>
-                                                            </div>
-                                                            <div className="mini-cart-item-des">
-                                                                <p>{catItem.title} </p>
-                                                                <span className="mini-cart-item-price">
-                                                                    ${catItem.price} x {" "} {catItem.qty}
-                                                                </span>
-                                                                <span className="mini-cart-item-quantity">
-                                                                    <button
-                                                                        onClick={() =>
-                                                                            props.removeFromCart(catItem.id)
-                                                                        }
-                                                                        className="btn btn-sm btn-danger"
-                                                                    >
-                                                                        <i className="ti-close"></i>
-                                                                    </button>{" "}
-                                                                </span>
-                                                            </div>
-                                                        </div>
-                                                    ))}
-                                            </div>
-                                            <div className="mini-cart-action clearfix">
-                                                <span className="mini-checkout-price">Subtotal: <span> ${totalPrice(carts)}</span></span>
-                                                <div className="mini-btn">
-                                                    <Link href="/checkout" className="view-cart-btn s1">Checkout</Link>
-                                                    <Link href="/cart" className="view-cart-btn">View Cart</Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div className="header-right" >
+                                    <Link onClick={ClickHandler} href="/login">Login</Link>
                                 </div>
                             </div>
                         </div>
@@ -129,9 +57,4 @@ const Header = (props) => {
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        carts: state.cartList.cart,
-    };
-};
-export default connect(mapStateToProps, { removeFromCart })(Header);
+export default (Header);
